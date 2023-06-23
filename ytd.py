ddda61e9193eea5ee7011efe8d2b6dcd21ff99fe
Wiljan666 <136391627+Wiljan666@users.noreply.github.com>
2023-06-24 00:22:41 +0200

    Update ytd.py

--- ytd.py
+++ ytd.py
@@ -1,32 +1,20 @@
-import streamlit as st
-from pytube import YouTube
 import os
+import subprocess
+import streamlit as st
+from pymsgbox import *
 
-def main():
-    path = st.text_input('Enter URL of any YouTube video')
-    option = st.selectbox(
-        'Select type of download',
-        ('audio', 'highest_resolution', 'lowest_resolution'))
+def verwijder_temp_bestanden():
+    temp_map = os.environ.get('TEMP')
+    if temp_map:
+        for root, dirs, files in os.walk(temp_map):
+            for bestand in files:
+                bestand_pad = os.path.join(root, bestand)
+                subprocess.run(['cmd', '/c', 'del', bestand_pad], shell=True)
+                st.write(f"Verwijderd: {bestand_pad}")
+            for map in dirs:
+                map_pad = os.path.join(root, map)
+                subprocess.run(['cmd', '/c', 'rmdir', '/s', '/q', map_pad], shell=True)
+                st.write(f"Verwijderd: {map_pad}")
+        alert(f"Bestanden in de %temp%-map zijn succesvol verwijderd.", "Succes")
 
-    if st.button("Download"):
-        video_object = YouTube(path)
-        st.write("Title of Video: " + str(video_object.title))
-        st.write("Number of Views: " + str(video_object.views))
-
-        download_dir = os.path.expanduser("~") + "\\Downloads"  # Het pad naar de "Downloads" map in Windows
-
-        if option == 'audio':
-            audio = video_object.streams.get_audio_only()
-            audio.download(output_path=download_dir)
-        elif option == 'highest_resolution':
-            video = video_object.streams.get_highest_resolution()
-            video.download(output_path=download_dir)
-        elif option == 'lowest_resolution':
-            video = video_object.streams.get_lowest_resolution()
-            video.download(output_path=download_dir)
-
-    if st.button("View"):
-        st.video(path)
-
-if __name__ == '__main__':
-    main()
+verwijder_temp_bestanden()
